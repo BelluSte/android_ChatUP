ec2d2bca988cfad437930b9ebbd4aa3c0a7ecaa2
BelluSte <62941381+BelluSte@users.noreply.github.com>
2020-05-22 16:49:22 +0200

    Layout ok

--- app/src/main/java/belluste/chatup/RegisterActivity.java
+++ app/src/main/java/belluste/chatup/RegisterActivity.java
@@ -6,19 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void clickSignIn(View view) {
     }
 
     public void clickLogin(View view) {
-    }
-
-    public void clickRegistrati(View view) {
-        Intent reg = new Intent(this, RegisterActivity.class);
-        startActivity(reg);
+        Intent login = new Intent(this, LoginActivity.class);
+        startActivity(login);
     }
 }
